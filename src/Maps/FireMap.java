95a0a8633ddcb2c961d6df3ad477ba7b9e28ad06
HunterYocum <Hunter.p.yocum@gmail.com>
2023-09-27 22:40:48 -0400

    Added Fire Tileset

--- src/Maps/FireMap.java
+++ src/Maps/FireMap.java
@@ -1,14 +1,14 @@
 package Maps;
 
 import Level.*;
-import Tilesets.EarthTileset; // change to FireTileset
+import Tilesets.FireTileset; // change to FireTileset
 import Utils.Point;
 
 public class FireMap extends Map{
     
     public FireMap(){
 
-        super("FireMap.txt", new EarthTileset()); // change to FireTileset
+        super("FireMap.txt", new FireTileset()); // change to FireTileset
         this.playerStartPosition = new Point(1, 11);
 
     }
--- src/Tilesets/FireTileset.java
+++ src/Tilesets/FireTileset.java
@@ -1,5 +1,46 @@
 package Tilesets;
 
-public class FireTileset {
-    
+import Builders.FrameBuilder;
+import Builders.MapTileBuilder;
+import Engine.ImageLoader;
+import GameObject.Frame;
+import GameObject.ImageEffect;
+import Level.TileType;
+import Level.Tileset;
+import Utils.SlopeTileLayoutUtils;
+
+import java.util.ArrayList;
+
+//This still needs to be completed in full - Hunter
+public class FireTileset extends Tileset {
+
+    public FireTileset() {
+        super(ImageLoader.load("FireSetGrid.png"), 16, 16, 3);
+    }
+
+    @Override
+    public ArrayList<MapTileBuilder> defineTiles() {
+        ArrayList<MapTileBuilder> mapTiles = new ArrayList<>();
+
+        // Lava Platform
+        Frame wallTileFrame = new FrameBuilder(getSubImage(1, 2))
+                .withScale(tileScale)
+                .build();
+
+        MapTileBuilder wallTile = new MapTileBuilder(wallTileFrame)
+                .withTileType(TileType.PASSABLE);
+
+        mapTiles.add(wallTile);
+ 
+        // Lava1
+        Frame lava1Frame = new FrameBuilder(getSubImage(1, 4))
+                .withScale(tileScale)
+                .build();
+
+        MapTileBuilder lava1Tile = new MapTileBuilder(lava1Frame);
+
+        mapTiles.add(lava1Tile);
+
+        return mapTiles;
+    }
 }
